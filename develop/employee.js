6a31bd18173447e08b9a2290342688f836d4b245
Andrew Ungvary <aungvary5@gmail.com>
2021-08-08 15:27:18 -0400

    adding directory structure and initial employee class

---
+++ develop/employee.js
@@ -0,0 +1,18 @@
+class Employee {
+    constructor() {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        console.log(`This employee's name is ${this.name}`)
+    }
+
+    getId() {
+        console.log(`This employee's id is ${this.id}`)
+    }
+}
+
+module.exports = Employee;
+
